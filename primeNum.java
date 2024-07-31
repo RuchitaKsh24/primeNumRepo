673e88d60424dc6c6683956ea1adc08de51af5e2
KanchanSuryavanshi29 <kanchusuryawanshi29072000@gmail.com>
2024-07-31 21:44:53 +0530

    commiting the primeNumber File

---
+++ primeNum.java
@@ -0,0 +1,24 @@
+public class Number {
+
+    public static boolean checkPrime(int num) {
+    
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int n = 23;
+        if (isPrimeNum(n) {
+            System.out.println(n+"is a prime number");
+        } else {
+            System.out.println(n + "is not a prime number");
+        }
+  }
+}
